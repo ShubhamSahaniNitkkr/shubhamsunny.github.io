0a14f991e3e1c87cd9a1c562def9174387bbb788
ShubhamSunny <shubhamsahaninitkkr@gmail.com>
2022-09-20 08:50:09 +0530

    - project added

---
+++ projects-data.js
@@ -0,0 +1,155 @@
+export const projects = [
+  {
+    images: "images/work/metamovers.gif",
+    title: "Metamovers (React)",
+    liveLink: "https://www.metamovers.io/",
+    desc:
+      "Dance NFT Showcase platform developed using React Js,CSS ,Strapi API",
+  },
+  {
+    images: "images/work/next-blog.gif",
+    title: "Blog (Next Js)",
+    desc:
+      "Advance Blog system Using Next js. The project is still in progress.",
+  },
+  {
+    images: "images/work/currency-calc.gif",
+    title: "Currency calculator (Typescript + Next Js)",
+    liveLink: "https://flourishing-flan-aca860.netlify.app/ ",
+    githubLink:
+      "https://github.com/ShubhamSahaniNitkkr/react-currency-converter",
+    desc:
+      "Simple Currency calculator using Typescript, Next Js , currencychanger api",
+  },
+  {
+    images: "images/work/confetti.gif",
+    title: "Confetti (React + CSS)",
+    githubLink: "https://github.com/Web-Appetize/confetti",
+    desc: "Simple Design Agency app for its organisation advertisement.",
+  },
+  {
+    images: "images/work/reactecommerce.gif",
+    title: "Employee portal (React + Material UI)",
+    desc:
+      "Simple Employee management tool (attendance , leaves , benefits etc), developed using React",
+  },
+  {
+    images: "images/work/reactecommerce.gif",
+    title: "Ecommerce (React)",
+    liveLink: "https://sunny-reactjs-ecommerce.netlify.com/",
+    githubLink: "https://github.com/ShubhamSahaniNitkkr/React_Ecommerce",
+    desc:
+      "Simple Ecommerce Using React, Add, Remove product to cart, checkout.",
+  },
+  {
+    images: "images/work/github_finder.gif",
+    title: "Github User Finder (React)",
+    liveLink: "https://sunnyreactjsgithubuserfinder.netlify.com/",
+    githubLink: "https://github.com/ShubhamSahaniNitkkr/githubfinder",
+    desc: "Simple React app to search github user using github-user API.",
+  },
+  {
+    images: "images/work/nodejs-api.png",
+    title: "User Authentication (Node)",
+    githubLink: "https://github.com/ShubhamSahaniNitkkr/node-api-shop",
+    desc: "Using node js , Express js , MongoDB , JWT for User Authentication.",
+  },
+  {
+    images: "images/work/laravelChatApp.gif",
+    title: "Realtime Chat App (socket.io)",
+    githubLink: "https://github.com/ShubhamSahaniNitkkr/chatApp2",
+    desc: "Realtime Chat App using laravel and socket.io",
+  },
+  {
+    images: "images/work/todo.gif",
+    title: "Todo App (React)",
+    liveLink: "https://sunny-todo-list-reactjs.netlify.com/",
+    githubLink: "https://github.com/ShubhamSahaniNitkkr/todo-list-app-reactjs",
+    desc: "Simple Todo App Using React, we can Add ,Modify, delete Task",
+  },
+  {
+    images: "images/work/nodejs_shoppingcart.gif",
+    title: "Shopping cart system (Node + React + MongoDB)",
+    githubLink:
+      "https://github.com/ShubhamSahaniNitkkr/shubham-sunny-shopping-cart",
+    desc: "Shopping cart system using React + node + MongoDB ",
+  },
+  {
+    images: "images/work/phpmysqlecommerce.gif",
+    title: "Ecommerce webapp (PHP) ",
+    githubLink: "https://github.com/ShubhamSahaniNitkkr/Ecommerce_2k_19",
+    desc:
+      "Complete Backend + Frontend of Ecommerce , with Admin panel. Using PHP + mysql ",
+  },
+  {
+    images: "images/work/Quiz.png",
+    title: "Quiz Platform (HTML + CSS + Js)",
+    githubLink: "https://github.com/ShubhamSahaniNitkkr/Quiz-Platform",
+    desc:
+      "Quiz Platform devloped using advance CSS with beautiful Design of phone using CSS ",
+  },
+  {
+    images: "images/work/MyApp.png",
+    title: "Andriod App Advertisment (HTML + CSS)",
+    githubLink: "https://github.com/ShubhamSahaniNitkkr/CSS-Android-App-Layout",
+    desc: "Beautiful Andriod App Advertisment webpage using using HTML + CSS ",
+  },
+  {
+    images: "images/work/LoginRegister.png",
+    title: "Login-Register-Utils (HTML + CSS + Js)",
+    githubLink: "https://github.com/ShubhamSahaniNitkkr/LoginRegisterUpdate",
+    desc: "Login , Register and Update User Information with PHP and AJAX ",
+  },
+  {
+    images: "images/work/JsFilter.png",
+    title: "Interactive UI (HTML + CSS + Js)",
+    githubLink: "https://github.com/ShubhamSahaniNitkkr/Js-Filteration",
+    desc: "Javascript Filter + Youtube API + Interactive UI",
+  },
+  {
+    images: "images/work/1.png",
+    title: "Js effecient realtime search (HTML + CSS + Js)",
+    githubLink: "https://github.com/ShubhamSahaniNitkkr/Js-Live-Search",
+    desc: "Fetch data from live api with effecient logic",
+  },
+  {
+    images: "images/work/Classroom.png",
+    title: "Classroom App (Js + HTML + CSS)",
+    githubLink:
+      "https://github.com/ShubhamSahaniNitkkr/Website-to-Manage-Students-For-Teachers",
+    desc:
+      "Basic Webapp for teachers to manage students (their performance , attendance , extra activity and skills) ",
+  },
+  {
+    images: "images/work/RnD.png",
+    title: "Collage R&D Website (HTML + CSS)",
+    githubLink:
+      "https://github.com/ShubhamSahaniNitkkr/R-D-Simple-Bootstrap-Website",
+    desc: "Webpage to show case of R&D of collage",
+  },
+  {
+    images: "images/work/Event.png",
+    title: "Event Management (HTML + CSS)",
+    githubLink:
+      "https://github.com/ShubhamSahaniNitkkr/Event-Management-Website",
+    desc: "Event management app for any organisation",
+  },
+  {
+    images: "images/work/Project.png",
+    title: "Project Management (HTML + CSS)",
+    githubLink: "https://github.com/ShubhamSahaniNitkkr/Project-Management",
+    desc: "Project management app for any organisation",
+  },
+  {
+    images: "images/work/Pokemon.png",
+    title: "Catch Pokemon (Angular)",
+    githubLink: "https://github.com/ShubhamSahaniNitkkr/AngularjsGuessPokemon",
+    desc: "Pokemon game",
+  },
+  {
+    images: "images/work/Games.png",
+    title: "Games Advertisement (HTML + CSS + Js)",
+    githubLink: "https://github.com/ShubhamSahaniNitkkr/Game_Advertisement",
+    desc: "Game advertisment webpage",
+  },
+];
